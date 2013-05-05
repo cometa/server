2b45e61df9da4aa96b597fa9caa20df5892f4d9d
Marco Graziano <marco@graziano.com>
2013-05-05 13:51:08 -0700

    added licensing information

--- Sinatra/appserver.rb
+++ Sinatra/appserver.rb
@@ -1,4 +1,23 @@
-#!/usr/bin/ruby
+#
+# Cometa is a cloud infrastructure for embedded systems and connected 
+# devices developed by Visible Energy, Inc.
+#
+# Copyright (C) 2013, Visible Energy, Inc.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# ruby appserver.rb
 #
 # A sample application server to interact with devices through the Cometa server.
 # 
@@ -9,6 +28,7 @@ require 'sinatra'
 require 'erb'
 require 'json'
 
+# Cometa Ruby server library
 require './lib/cometa-ruby.rb'
 
 #
